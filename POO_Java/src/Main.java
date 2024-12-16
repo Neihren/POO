e49dcdf0bdd5334c7d7fa51f3bfe50b0a020a86d
Neihren <185206470+Neihren@users.noreply.github.com>
2024-12-16 17:00:12 +0100

    java

--- POO_Java/src/Main.java
+++ POO_Java/src/Main.java
@@ -1,15 +1,32 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         
+        // Instanciation de l'objet b0 de la classe Bouteille par constructeur par défaut
+        Bouteille b0 = new Bouteille();
+        // Instanciation de l'objet b1 de la classe Bouteille par constructeur classique
         Bouteille b1 = new Bouteille(true, 100, 10);
-        b1.ouvrir();
+        // Instanciation de l'objet b2 de la classe Bouteille par constructeur par clonage
         Bouteille b2 = (Bouteille) b1.clone();
+        
+        // Affichage des etats de l'objet b1 avec la méthode toString()
         System.out.println(b1.toString());
-        b1.remplir(70);
-        System.out.println(b1.toString());
+        
+        // Fermeture de l'objet b1 avec la méthode fermer()
+        b1.fermer();
 
-        b1.vider(50);
-        System.out.println(b1.getContenuEnPourcentage());
-        System.out.println(b2.toString());
+        // Ouverture de l'objet b1 avec la méthode ouvrir()
+        b1.ouvrir();
+
+        // Remplissage de l'objet b1 avec la méthode remplir()
+        b1.remplir();
+
+        // Vidage de l'objet b1 avec la méthode vider()
+        b1.vider();
+
+        // Remplissage partiel de l'objet b1 avec la méthode remplir(float pourcentageARemplir)
+        b1.remplir(50);
+
+        // Vidage partiel de l'objet b1 avec la méthdode vider(float pourcentageAVider)
+        b1.vider(20);
     }
 }
